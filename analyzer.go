8d311793c3a6ec1eeee6c45ca6267d9e379610d4
nobishino <nobishii11@gmail.com>
2022-05-28 10:13:03 +0900

    write skeleton of tokenizer

--- analyzer.go
+++ analyzer.go
@@ -1,1 +1,6 @@
 package jackc
+
+// source: Xxx.jack / directory name
+// output: Xxx.xml
+// create JackTokenizer
+// use Compilation Engine
--- tokenizer.go
+++ tokenizer.go
@@ -17,5 +17,78 @@ func (t *Tokenizer) HasMoreTokens() bool {
 // 入力から次のトークンを取得し、それを現在のトークン(現トークン)する。
 // このルーチンは、hasMoreTokens()がTrueの場合のみ呼び出すことができる。
 // また、最初は現トークンは設定されていない
+func (t *Tokenizer) Advance() {}
 
 // 現トークンの種類を返す
+func (t *Tokenizer) TokenType() TokenType {
+	var v TokenType
+	return v
+}
+
+// 現トークンのキーワードを返す。このルーチンは、tokenType()がKEYWORDの場合のみ呼び出すことができる
+func (t *Tokenizer) KeyWord() Keyword {
+	var v Keyword
+	return v
+}
+
+// 現トークンの文字を返す。このルーチンは、tokenType()がSYMBOLの場合に呼び出すことができる
+func (t *Tokenizer) Symbol() Symbol {
+	var s Symbol
+	return s
+}
+
+// Identifierは、現トークンの識別子を返す。tokenType()がÍIDENTIFIERの場合のみ呼び出せる
+func (t *Tokenizer) Identifier() string {
+	return ""
+}
+
+// Intval は、現トークンの整数の値を返す。tokenType() == INT_VALのときのみ呼び出せる。
+func (t *Tokenizer) IntVal() int {
+	return 0
+}
+
+// StringVal は、現トークンの文字列値をかえす。tokenType() == STRING_VALのときのみ呼び出せる。
+func (t *Tokenizer) StringVal() string {
+	return ""
+}
+
+// TokenType represents token types
+type TokenType string
+
+const (
+	KEYWORD      TokenType = "KEYWORD"
+	SYMBOL       TokenType = "SYMBOL"
+	IDENTIFIER   TokenType = "IDENTIFIER"
+	INT_CONST    TokenType = "INT_CONST"
+	STRING_CONST TokenType = "STRING_CONST"
+)
+
+// Keyword represents keywords
+type Keyword string
+
+const (
+	CLASS       Keyword = "CLASS"
+	METHOD      Keyword = "METHOD"
+	FUNCTION    Keyword = "FUNCTION"
+	CONSTRUCTOR Keyword = "CONSTRUCTOR"
+	INT         Keyword = "INT"
+	BOOLEAN     Keyword = "BOOLEAN"
+	CHAR        Keyword = "CHAR"
+	VOID        Keyword = "VOID"
+	VAR         Keyword = "VAR"
+	STATIC      Keyword = "STATIC"
+	FIELD       Keyword = "FIELD"
+	LET         Keyword = "LET"
+	DO          Keyword = "DO"
+	IF          Keyword = "IF"
+	ELSE        Keyword = "ELSE"
+	WHILE       Keyword = "WHILE"
+	RETURN      Keyword = "RETURN"
+	TRUE        Keyword = "TRUE"
+	FALSE       Keyword = "FALSE"
+	NULL        Keyword = "NULL"
+	THIS        Keyword = "THIS"
+)
+
+// Symbol represents symbols
+type Symbol rune
