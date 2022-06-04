ab81a4b71197abc3f3de95cb9643984c24db09a6
nobishino <nobishii11@gmail.com>
2022-06-04 11:46:57 +0900

    WIP

--- tokenizer_test.go
+++ tokenizer_test.go
@@ -17,12 +17,12 @@ func TestTokenizer(t *testing.T) {
 		{`method`, 1},
 		{`}`, 1},
 		{`{}`, 2},
-		{`Class Bar {
-	method Fraction foo(int y) {
-		var int temp;
-		let temp = (xxx+12)*-63;
-	}
-}`, 4},
+		// 		{`Class Bar {
+		// 	method Fraction foo(int y) {
+		// 		var int temp;
+		// 		let temp = (xxx+12)*-63;
+		// 	}
+		// }`, 4},
 	}
 
 	for _, tc := range cases {
